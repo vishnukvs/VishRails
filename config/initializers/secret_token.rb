490f04c510f3ed93054bdb6896695fea2fc01523
Vishnu Kanduri <vishnu.kvs@gmail.com>
2014-02-21 09:32:53 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VishnuBlog::Application.config.secret_key_base = 'dac8f6d126cb8de691a42ff6b60c382832679d0fddf97c1bb4b5a73afd1fa0b193a20e6501bc900e106ebb8fff2195f869887bf20a065f593de4d6e02dace6eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VishnuBlog::Application.config.session_store :cookie_store, key: '_vishnu_blog_session'
